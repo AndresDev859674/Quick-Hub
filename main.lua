10cb1d8544d69f2c217b74e863caa892fcb4fe28
Andrew <167466992+AndresDev859674@users.noreply.github.com>
2025-01-06 18:37:16 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
-﻿local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Quick Scripts Hub V2",
