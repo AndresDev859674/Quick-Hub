3fe1f8c5d174860f468b7106a86c2295db761b39
Andrew <167466992+AndresDev859674@users.noreply.github.com>
2025-01-07 19:50:35 -0400

    Update execute.lua

--- execute.lua
+++ execute.lua
@@ -1,2 +1,20 @@
-local scriptContent = game:HttpGet("https://raw.githubusercontent.com/AndresDev859674/Quick-Scripts-Hub-V2/main/main.lua")
-loadstring(scriptContent)()
+print("Loading Quick Scripts Hub V2.....")
+
+local success, scriptContent = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/AndresDev859674/Quick-Scripts-Hub-V2/main/main.lua")
+end)
+
+if success then
+    print("Script content loaded successfully.")
+    local loadSuccess, loadError = pcall(function()
+        loadstring(scriptContent)()
+    end)
+    
+    if loadSuccess then
+        print("Script executed successfully.")
+    else
+        warn("Error executing script: " .. loadError)
+    end
+else
+    warn("Error loading script content.")
+end
