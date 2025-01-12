3d62f8a4acfcf95a30731a32673905ae12fb0885
Andrew <167466992+AndresDev859674@users.noreply.github.com>
2025-01-12 19:59:18 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -8,12 +8,12 @@ local Tab = Window:NewTab("Welcome")
 
 local Section = Tab:NewSection("Welcome to Quick Center Loader Menu!")
 
-local button = Section:NewButton("Quick Scripts Hub Modern (Recomended and Main)", "ButtonInfo", function()
+local button = Section:NewButton("Quick Scripts Hub Modern (Recomended and Main)", "Opens QSH V2 and Close this Window", function()
     local scriptContent = game:HttpGet("https://raw.githubusercontent.com/AndresDev859674/Quick-Scripts-Hub-V2/main/main.lua")
     loadstring(scriptContent)()
 end)
 
-local button = Section:NewButton("Quick Functions Hub (NEW!)", "ButtonInfo", function()
+local button = Section:NewButton("Quick Functions Hub (NEW!)", "Opens QFH and Close this Window", function()
     local scriptContent = game:HttpGet("https://raw.githubusercontent.com/AndresDev859674/Quick-Scripts-Hub-V2/main/nextgen.lua")
     loadstring(scriptContent)()
 end)
