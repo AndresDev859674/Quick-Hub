16cf7ce00ccaca9b3fb1f343ff72d77a0312401e
Andrew <167466992+AndresDev859674@users.noreply.github.com>
2025-01-12 18:57:43 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,16 @@
+print("Running First Quick Scripts Hub Loader")
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("Quick Scripts Hub Loader", "DarkTheme")
+
+local Tab = Window:NewTab("Welcome")
+
+local Section = Tab:NewSection("Welcome to Quick Scripts Hub Loader Menu!")
+
+local button = Section:NewButton("Quick Scripts Hub Modern (Recomended and Main)", "ButtonInfo", function()
+    local scriptContent = game:HttpGet("https://raw.githubusercontent.com/AndresDev859674/Quick-Scripts-Hub-V2/main/execute.lua")
+    loadstring(scriptContent)()
+
+    button:Destroy()
+end)
